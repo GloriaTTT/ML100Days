0eaf69ca3849b177ad03a158759f737e4d06c47e
GloriaTTT <77912860+GloriaTTT@users.noreply.github.com>
2021-01-24 16:44:52 +0800

    Create 20210119.py

---
+++ 20210119.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import numpy as np
+a = np.arange(0,21,1)
+print("a=",a)
+b = np.arange(0,21,2)
+print("b=",b)
+c = np.arange(3,21,3)
+print("c=",c)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
